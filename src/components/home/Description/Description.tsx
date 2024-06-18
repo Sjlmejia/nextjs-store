b08a1d5b653c62919b2ef3c8c3adf3bb97f1b6c5
Jorge Mejia <jorgemejia2304@gmail.com>
2024-06-18 16:56:37 -0500

    fix css

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -11,8 +11,10 @@ export function Description() {
           fallbackSrc='https://via.placeholder.com/150'
         />
     </Center>
+    <Box m={20}>
     <h2>Main Products</h2>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A minima, magni ipsa dignissimos veniam maxime architecto et quo deserunt unde corrupti cupiditate debitis accusantium non quasi saepe provident hic fuga!</p>
+    </Box>
   </section>
   )
 }
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.css'
+import { Box } from '@chakra-ui/react';
 const getProducts = async () => {
   
   try {
@@ -19,20 +20,20 @@ const getProducts = async () => {
 async function MainProducts() {
   const products = await getProducts();
   return (
-    <section className={styles.MainProducts}>
+    <Box m={10} className={styles.MainProducts}>
     <h3>✨ New products released!</h3>
     <div className={styles.MainProducts__grid}>
       {products?.map((product:any) => {
         const imageSrc = product.images[0].src;
         return (
-          <article key={product.id}>
+          <div className={styles.Item} key={product.id}>
             <p>{product.title}</p>
-            <Image src={imageSrc} fill alt={product.title} loading="eager" />
-          </article>
+            <Image className={styles.Image} src={imageSrc} fill alt={product.title} loading="eager" />
+          </div>
         )
       })}
     </div>
-  </section>
+  </Box>
   )
 }
 
